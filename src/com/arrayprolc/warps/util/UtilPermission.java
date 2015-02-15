7798bcc1cc1645ce630dd6d093736989ecce819f
ArrayPro <arraypro07@gmail.com>
2015-02-15 18:16:48 -0500

    Added an "allowedDefault" configuration setting:
    Seperated by commas, a list of permissions inside SmoothWarps that any
    player can access (omit the "warps." prefix)

--- src/com/arrayprolc/warps/util/UtilPermission.java
+++ src/com/arrayprolc/warps/util/UtilPermission.java
@@ -2,6 +2,8 @@ package com.arrayprolc.warps.util;
 
 import org.bukkit.entity.Player;
 
+import com.arrayprolc.warps.Warps;
+
 public class UtilPermission {
 
 	private static String prefix = "warps.";
@@ -14,6 +16,25 @@ public class UtilPermission {
 			if (p.hasPermission("warps.warp.*")) {
 				return true;
 			}
+		}
+		try {
+			String list = Warps.getInstance().getConfig()
+					.getString("allowedDefault");
+			String[] allowed = list.split(",");
+			for (String s : allowed) {
+				if (permission.startsWith("warp.")) {
+					if (s.equalsIgnoreCase("warps.warp.*")) {
+						return true;
+					}
+				}
+				if (s.equalsIgnoreCase(permission)) {
+					return true;
+				}
+			}
+		} catch (Exception ex) {
+			// It's probably a NPE, that's okay.
+			Warps.getInstance().getConfig().set("allowedDefault", "info.*");
+			Warps.getInstance().saveConfig();
 		}
 		return p.hasPermission(prefix + permission);
 
